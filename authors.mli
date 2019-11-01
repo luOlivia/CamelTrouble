17c299ca94a016b938efc17283beb6fb740b1360
Olivia <ol48@cornell.edu>
2019-11-01 13:56:19 -0400

    authors

---
+++ authors.mli
@@ -0,0 +1,8 @@
+(**
+   Metadata about this submission.
+   @author Olivia Lu (ol48), Lillian Hong (ch745), Abhay Singh (as2626). 
+*)
+
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list

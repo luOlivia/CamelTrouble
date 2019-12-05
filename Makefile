145cd4fcd68fe627d10f196dbf2f9d387e9645da
Lillian Hong <ch756@cornell.edu>
2019-12-05 02:05:38 -0500

    Js of ocaml sort of working!

--- Makefile
+++ Makefile
@@ -1,27 +1,26 @@
-MODULES=ball camel cell main maze state test
+MODULES=ball camel cell main maze state test position resources 
 OBJECTS=$(MODULES:=.cmo)
 MLS=$(MODULES:=.ml)
 MLIS=$(MODULES:=.mli)
-TEST=test.byte
+# TEST=test.byte
 MAIN=main.byte
 OCAMLBUILD=ocamlbuild -use-ocamlfind
-PKGS=unix,oUnit,str,graphics
 
 
 default: build
 	utop
 
 build:
-	$(OCAMLBUILD) $(OBJECTS) && js_of_ocaml +graphics.js $(MAIN)
+	$(OCAMLBUILD) $(MAIN) && js_of_ocaml +graphics.js $(MAIN) 
 	
-test:
-	$(OCAMLBUILD) $(TEST) && ./$(TEST)
+# test:
+# 	$(OCAMLBUILD) $(TEST) && ./$(TEST)
 
 play:
 	$(OCAMLBUILD) $(MAIN) && ./$(MAIN)
 
-zip:
-	zip cameltrouble.zip *.ml* _tags *.txt *.js *.md *.byte Makefile
+# zip:
+# 	zip cameltrouble.zip *.ml* _tags *.txt *.js *.md *.byte Makefile
 
 docs: docs-public docs-private
 
--- main.ml
+++ main.ml
@@ -5,12 +5,16 @@ open Maze
 open Graphics_js
 open Unix 
 
+
 (* let fake_maze = make_maze 20
    let fake_maze = make_maze 20
    let fake_maze = make_maze 20
    let fake_maze = make_maze 20 *)
+let () = print_endline "passed here
+dsaaaaaaaaaaaa
+asdddddddddddd"
 
-let () = print_endline (State.init_state.maze |> Maze.to_str)
+(* let () = print_endline (State.init_state.maze |> Maze.to_str) *)
 
 type keys = {
   mutable p1_up : bool;
@@ -40,7 +44,7 @@ let input_keys = {
 (** [draw_camel camel color] is the [camel] drawn 
     on maze in direction [dir] *)
 let draw_camel camel color =
-  set_color color;
+  Graphics_js.set_color color;
   let w = 10.0 in
   let h = camel_width in
   let d = camel.dir in
@@ -53,25 +57,26 @@ let draw_camel camel color =
   let bl_x, bl_y = State.rot_point (cx-.w/.2.) (cy+.h/.2.) cx cy d in
 
   (* print_endline ("points "^"("^string_of_int tl_x^","^string_of_int tl_y^")"); *)
-  fill_poly [|(br_x,br_y);(bl_x,bl_y);(tl_x,tl_y);(tr_x,tr_y)|]
+  Graphics_js.fill_poly [|(br_x,br_y);(bl_x,bl_y);(tl_x,tl_y);(tr_x,tr_y)|]
+(* Graphics_js.fill_rect (int_of_float cx) (int_of_float cy) (int_of_float w) (int_of_float h) *)
 
 let draw_balls state color = 
-  set_color color;
+  Graphics_js.set_color color;
 
   let rec iter_balls = function
     | [] -> () 
-    | ball::t -> fill_circle (ball.position.x |> int_of_float) (ball.position.y |> int_of_float) 3; iter_balls t 
+    | ball::t -> Graphics_js.fill_circle (ball.position.x |> int_of_float) (ball.position.y |> int_of_float) 3; iter_balls t 
   in iter_balls state.ball_list 
 
 let draw_state state = 
   (* set background color to sand *)
-  (* set_color (Graphics_js.rgb 255 251 224);
-     fill_rect 0 0 xDimension yDimension; *)
+  (* Graphics_js.set_color (Graphics_js.rgb 255 251 224);
+     Graphics_js.fill_rect 0 0 xDimension yDimension; *)
 
   (* draws walls *)
   let halfw = State.wall_width/.2. in 
   let halfh = State.wall_height/.2. in 
-  set_color (Graphics_js.rgb 4 145 23);
+  Graphics_js.set_color (Graphics_js.rgb 4 145 23);
 
   for i = 0 to 6 do
     for j = 0 to 6 do
@@ -79,12 +84,12 @@ let draw_state state =
       let hlx = (State.square_width +. State.wall_width)*.(i |> float_of_int) +. State.wall_width in 
       let hly = (State.square_width +. State.wall_width)*.((j |> float_of_int)+.1.)  in
       if Maze.is_wall_below state.maze i j then 
-        fill_rect 
+        Graphics_js.fill_rect 
           (hlx |> int_of_float) (hly |> int_of_float) 
           (State.wall_height |> int_of_float) (State.wall_width |> int_of_float);
 
       if j = 0 then 
-        fill_rect 
+        Graphics_js.fill_rect 
           (hlx |> int_of_float) (hly -. State.square_width -. State.wall_width |> int_of_float) 
           (State.wall_height |> int_of_float) (State.wall_width |> int_of_float);
 
@@ -93,12 +98,12 @@ let draw_state state =
       let vly = (State.square_width +. State.wall_width)*.(j |> float_of_int)+. State.wall_width in
 
       if Maze.is_wall_right state.maze i j then
-        fill_rect 
+        Graphics_js.fill_rect 
           (vlx |> int_of_float) (vly |> int_of_float) 
           (State.wall_width |> int_of_float) (State.wall_height |> int_of_float);
 
       if i = 0 then 
-        fill_rect 
+        Graphics_js.fill_rect 
           (vlx -. State.square_width -. State.wall_width |> int_of_float) (vly  |> int_of_float) 
           (State.wall_width |> int_of_float) (State.wall_height |> int_of_float);
 
@@ -113,8 +118,9 @@ let draw_state state =
   draw_balls state Graphics_js.black
 
 let keypressed evt =
-  let () = match evt##keyCode with
-    | 38  -> input_keys.p2_up<- true
+  print_endline "TRIGGERING HERE =======================";
+  let () = match evt##.keyCode with
+    | 38  -> print_endline "hello there bitch";  input_keys.p2_up<- true
     | 39 -> input_keys.p2_right <- true
     | 37 -> input_keys.p2_left <- true
     | 40 -> input_keys.p2_down <- true
@@ -129,7 +135,7 @@ let keypressed evt =
 
 (* Keyup event handler translates a key release *)
 let keyup evt =
-  let () = match evt##keyCode with
+  let () = match evt##.keyCode with
     | 38  -> input_keys.p2_up<- false
     | 39 -> input_keys.p2_right <- false
     | 37 -> input_keys.p2_left <- false
@@ -173,13 +179,13 @@ let flush_kp () = while key_pressed () do
 let input d = d 
 (** [run] displays the game window and allows users to quit with key 0
     refactor later bc alex *)
-let rec run state =
-  let new_state = input state in
-  Graphics_js.auto_synchronize false;
-  Graphics_js.clear_graph (); 
-  draw_state new_state;
-  Graphics_js.auto_synchronize true;
-  run new_state
+(* let rec run state =
+   let new_state = input state in
+   (* Graphics_js.auto_synchronize false; *)
+   (* Graphics_js.clear_graph ();  *)
+   draw_state new_state;
+   (* Graphics_js.auto_synchronize true; *)
+   run new_state *)
 
 
 
@@ -190,29 +196,35 @@ let rec run state =
    run State.init_state  *)
 
 let init () = 
-  draw_state State.init_state; run State.init_state
+  print_endline "we in init now bois and girls";
+  draw_state State.init_state
+(* ;run State.init_state *)
 
 let main () = 
-  (* Graphics_js.open_graph ""; *)
-  set_window_title "Camel Trouble";
-  resize_window (State.xDimension) (State.yDimension);
-  Graphics_js.set_text_size 300; 
-  Graphics_js.moveto 50 500;
-  Graphics_js.draw_string "WELCOME TO";
-  Resources.draw "desert" 10 300;
-  Graphics_js.draw_string "PRESS ANY KEY TO PLAY";
-  match Graphics_js.read_key () with 
-  | _ -> init ()
 
-let _ = Js_of_ocaml.Js.Opt.iter
+  (* Graphics_js.set_text_size 300; 
+     Graphics_js.moveto 50 500; *)
+  (* Graphics_js.draw_string "WELCOME TO"; *)
+  (* Resources.draw "desert" 10 300; *)
+  (* Graphics_js.draw_string "PRESS ANY KEY TO PLAY"; *)
+  (* match Graphics_js.read_key () with 
+     | _ -> init () *)
+  init ()
+
+let _ =  print_endline "starting up";
+  Js_of_ocaml.Js.Opt.iter
     (Js_of_ocaml.Dom_html.CoerceTo.canvas (Js_of_ocaml.Dom_html.getElementById "canvas2"))
     Graphics_js.open_canvas
 
-(* let c1 =
-   Dom_html.addEventListener
-    Dom_html.document
-    Dom_html.Event.keydown
-    (Dom_html.handler (fun ev ->
+let _ = Js_of_ocaml.Dom_html.addEventListener Js_of_ocaml.Dom_html.document Js_of_ocaml.Dom_html.Event.keydown (Js_of_ocaml.Dom_html.handler keypressed) Js_of_ocaml.Js._true 
+let _ = Js_of_ocaml.Dom_html.addEventListener Js_of_ocaml.Dom_html.document Js_of_ocaml.Dom_html.Event.keyup (Js_of_ocaml.Dom_html.handler keyup) Js_of_ocaml.Js._true 
+
+(* 
+let c1 =
+  Js_of_ocaml.Dom_html.addEventListener
+    Js_of_ocaml.Dom_html.document
+    Js_of_ocaml.Dom_html.Event.keydown
+    (Js_of_ocaml.Dom_html.handler (fun ev ->
          let _ = match ev##.keyCode with
            | 32 -> input := Board.HardDrop
            | 90 -> input := Board.Rotate(false)
@@ -223,9 +235,8 @@ let _ = Js_of_ocaml.Js.Opt.iter
            | 66|40 -> input := Board.FastDrop
            | _ -> ();
          in
-         Js._true ))
-    Js._true
-*)
+         Js_of_ocaml.Js._true ))
+    Js_of_ocaml.Js._true *)
 
 
-(* let () = main () *)
+let () = main ()
